807d866e5fb881fd151fa60229019ce09f590668
Jie Chen <janicec2046@gmail.com>
2024-06-29 18:01:26 -0400

    setup

---
+++ api_summarize/global.R
@@ -0,0 +1,11 @@
+# Load necessary libraries
+library(shiny)
+library(httr)
+library(tidyverse)
+library(jsonlite)
+
+
+#fetch_api_data <- fromJSON(
+#  "https://api.fda.gov/tobacco/problem.json?&limit=1000"
+#)
+#output <- fetch_api_data$results
